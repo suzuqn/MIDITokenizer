7da635f647b947b67dd2bfe2fb2d7165778f5235
Masahiro Suzuki <92901486+suzuqn@users.noreply.github.com>
2022-07-29 14:43:02 +0900

    Supports measure range specification

--- midi_to_tokens.py
+++ midi_to_tokens.py
@@ -1,35 +1,44 @@
+import copy
 import pretty_midi
 
 class midi_to_tokens():
     def __init__(self, path, steps_per_beat=12):
         self.steps_per_beat = steps_per_beat
         self.pm = pretty_midi.PrettyMIDI(path)
+        self.dbs = self.pm.get_downbeats().tolist() + [self.pm.get_end_time()] # dbs := downbeats
+        self.tokens = self._tokenize()
         
     def __call__(self):
         return ' '.join(self.tokens)
         
-    def time_to_step(self, time):
+    def _time_to_step(self, time):
         return round(self.pm.time_to_tick(time) / self.pm.resolution * self.steps_per_beat)
     
-    def event_to_tokens(self, event):
+    def _event_to_tokens(self, event):
         if event in ('bar', 'beat'):
             return [event]
         elif isinstance(event, pretty_midi.containers.Note):
-            return [f'note_{event.pitch}', f'len_{self.time_to_step(event.end - event.start)}']
+            return [f'note_{event.pitch}', f'len_{self._time_to_step(event.end - event.start)}']
+        
+    def _trim_note(self, note, start, end):
+        n = copy.copy(note)
+        n.start, n.end = max(n.start, start), min(n.end, end)
+        return n
     
-    @property
-    def tokens(self):
+    def _tokenize(self, start_measure=1, end_measure=None):
+        start, end = self.dbs[start_measure - 1], self.dbs[end_measure or -1]
+        
         notes = []
         for inst in self.pm.instruments:
             notes += inst.notes
         notes.sort(key=lambda x: (x.start, -x.pitch))
 
         events = []
-        events += [(self.time_to_step(db), 'bar') for db in self.pm.get_downbeats()]
-        events += [(self.time_to_step(b), 'beat') for b in set(self.pm.get_beats()) - set(self.pm.get_downbeats())] # beats except for downbeats
-        events += [(self.time_to_step(n.start), n) for n in notes]
+        events += [(self._time_to_step(db), 'bar') for db in self.dbs if start <= db < end]
+        events += [(self._time_to_step(b), 'beat') for b in set(self.pm.get_beats()) - set(self.dbs) if start <= b < end] # beats without downbeats
+        events += [(self._time_to_step(max(n.start, start)), self._trim_note(n, start, end)) for n in notes if start <= n.start < end or start < n.end <= end]        
         events.sort(key=lambda x: x[0])
-
+        
         tokens = []
         last_beat = 0
         for step, event in events:
@@ -37,6 +46,9 @@ class midi_to_tokens():
                 last_beat = step
             if step - last_beat:
                 tokens.append(f'pos_{step - last_beat}')
-            tokens += self.event_to_tokens(event)
+            tokens += self._event_to_tokens(event)
 
         return tokens
+    
+    def measures(self, start_measure=1, end_measure=None):
+        return self._tokenize(start_measure, end_measure)
